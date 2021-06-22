159bbbb386b7406809c6ee65a382567ec796a060
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-06-22 19:47:48 +0900

    アルゴリズムの基本学習

--- Algorithm/Sort/insertion_sort.py
+++ Algorithm/Sort/insertion_sort.py
@@ -4,11 +4,18 @@ from typing import List
 
 
 def sort_by_insertion(unsorted_list: List[int]) -> List[int]:
+    """
+    挿入ソート
+
+    :param unsorted_list: 対象リスト
+    :return: ソート済みリスト
+    """
     sort_list = list.copy(unsorted_list)
 
     for i in range(0, len(sort_list) - 1):
         for j in range(i + 1, 0, -1):
 
+            # 対象要素 < ソート済みの要素の場合は、昇順となるよう入れ替えが必要
             if sort_list[j] < sort_list[j - 1]:
                 sort_list[j], sort_list[j - 1] = sort_list[j - 1], sort_list[j]
 
