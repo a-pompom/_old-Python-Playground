98ebcba1188e0f3ade783134683d56036b43909a
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-07-17 15:28:51 +0900

    プレイヤークラスの定数を辞書からデータクラスへリファクタ
    定数を辞書ではなくデータクラスで表現することで、データ専用であることを強調し、補完のメリットを得られるようにした。

--- app_tick_tack_toe/player.py
+++ app_tick_tack_toe/player.py
@@ -1,21 +1,29 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class Candidate:
+    """ 現在プレイヤーの候補 """
+    PLAYER_1: str = 'FIRST'
+    PLAYER_2: str = 'SECOND'
+
+
+@dataclasses.dataclass
+class Mark:
+    """ 盤面へ印字する記号 """
+    FIRST: str = '○'
+    SECOND: str = '×'
+
+
 class Player:
     """ ゲームのプレイヤーの状態管理を責務に持つ """
 
-    # 現在プレイヤーの候補
-    CANDIDATE = {
-        'player1': '1p',
-        'player2': '2p'
-    }
-
-    # 盤面へ印字する記号
-    MARK = {
-        '1p': '○',
-        '2p': '×'
-    }
+    CANDIDATE: Candidate = Candidate()
+    MARK: Mark = Mark()
 
     def __init__(self):
         # 現在プレイヤー
-        self.current = self.CANDIDATE['player1']
+        self.current = self.CANDIDATE.PLAYER_1
 
     def get_mark(self):
         """
@@ -24,15 +32,15 @@ class Player:
         :return: ○ or ×
         """
 
-        return self.MARK[self.current]
+        return self.MARK.__getattribute__(self.current)
 
     def change(self):
         """
         プレイヤーを切り替え
         """
 
-        if self.current == self.CANDIDATE['player1']:
-            self.current = self.CANDIDATE['player2']
+        if self.current == self.CANDIDATE.PLAYER_1:
+            self.current = self.CANDIDATE.PLAYER_2
             return
 
-        self.current = self.CANDIDATE['player1']
+        self.current = self.CANDIDATE.PLAYER_1
